d86b417f6105d0cb0598534f76cb1c8089ab2297
Stellnl <71969546+Stellnl@users.noreply.github.com>
2021-10-10 17:28:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // API reference URL
-    apiUrl: "",
+    apiUrl: "https://eu0.chat-api.com/instance12345/",
     // API token from your personal cabinet
-    token: ""
+    token: "kSgmBA0NtsbVfbhPkxsR8sPydO43"
 }
